551ef6bb879c13a9bf04be763e2dffbeb30f98ce
NathanRuprecht <nathan.a.ruprecht@gmail.com>
2016-10-22 11:21:19 -0500

    Add files via upload

---
+++ Lab2/Minimum_Functionality.asm
@@ -0,0 +1,107 @@
+;-------------------------------------------------------------
+; Lab 2 - Subroutines - Cryptography
+; C2C Nathan Ruprecht, USAF / 14 Sept 2014 / 19 Sept 2014
+;
+;Program works for all cases tested
+;
+; This program is a demonstration of using the CCS IDE to
+; program, assemble, flash, and debug the MSP430.
+;-------------------------------------------------------------
+
+
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section
+            .retainrefs                     ; Additionally retain any sections
+                                            ; that have references to current
+                                            ; section
+            .global main
+            .data
+			.bss decrypted_message, 0x40
+            .text
+
+message:    .byte    0xef, 0xc3,0xc2,0xcb,0xde,0xcd,0xd8,0xd9,0xc0,0xcd,0xd8,0xc5,0xc3,0xc2,0xdf,0x8d,0x8c,0x8c,0xf5,0xc3,0xd9,0x8c,0xc8,0xc9,0xcf,0xde,0xd5,0xdc,0xd8,0xc9,0xc8,0x8c,0xd8,0xc4,0xc9,0x8c,0xe9,0xef,0xe9,0x9f,0x94,0x9e,0x8c,0xc4,0xc5,0xc8,0xc8,0xc9,0xc2,0x8c,0xc1,0xc9,0xdf,0xdf,0xcd,0xcb,0xc9,0x8c,0xcd,0xc2,0xc8,0x8c,0xcd,0xcf,0xc4,0xc5,0xc9,0xda,0xc9,0xc8,0x8c,0xde,0xc9,0xdd,0xd9,0xc5,0xde,0xc9,0xc8,0x8c,0xca,0xd9,0xc2,0xcf,0xd8,0xc5,0xc3,0xc2,0xcd,0xc0,0xc5,0xd8,0xd5,0x8f
+
+key:		.byte    0x00ac
+;-------------------------------------------------------------------------------
+main:       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+	     	mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+;-------------------------------------------------------------------------------
+                                            ; Main loop here
+;-------------------------------------------------------------------------------
+			mov #decrypted_message, R12
+            mov #message, R5
+            mov #key, R6
+            mov #0x5E, R7 ;count <- 94
+
+            call    #decryptMessage
+
+forever:    jmp     forever
+
+;-------------------------------------------------------------------------------
+                                            ; Subroutines
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+;Subroutine Name: decryptMessage
+;Author: C2C Nathan Ruprecht
+;Function: Decrypts a string of bytes and stores the result in memory.  Accepts
+;           the address of the encrypted message, address of the key, and address
+;           of the decrypted message (pass-by-reference).  Accepts the length of
+;           the message by value.  Uses the decryptCharacter subroutine to decrypt
+;           each byte of the message.  Stores theresults to the decrypted message
+;           location.
+;Inputs:
+;Outputs:
+;Registers destroyed:
+;-------------------------------------------------------------------------------
+
+decryptMessage:
+loop		cmp #0x0, R7
+			jz forever
+			call #decryptCharacter
+			dec R7
+			jmp loop
+
+            ret
+
+;-------------------------------------------------------------------------------
+;Subroutine Name: decryptCharacter
+;Author: C2C Nathan Ruprecht
+;Function: Decrypts a byte of data by XORing it with a key byte.  Returns the
+;           decrypted byte in the same register the encrypted byte was passed in.
+;           Expects both the encrypted data and key to be passed by value.
+;Inputs:
+;Outputs:
+;Registers destroyed:
+;-------------------------------------------------------------------------------
+
+decryptCharacter:
+			mov.b @R5+, R10
+			xor.b @R6, R10
+			mov R10, 0(R12)
+			inc.w R12
+			inc.w R12
+
+            ret
+
+;-------------------------------------------------------------------------------
+;           Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect    .stack
+
+;-------------------------------------------------------------------------------
+;           Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  main
